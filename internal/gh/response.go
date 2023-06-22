7c8613f8d2725325a0505681e17eb0e1aeaf1cce
kumak1 <makino@pepabo.com>
2023-06-22 23:37:59 +0900

    add gh response struct

---
+++ internal/gh/response.go
@@ -0,0 +1,29 @@
+package gh
+
+type Event struct {
+	Id   string
+	Type string
+	Repo struct {
+		Name string
+		Url  string
+	}
+	Payload struct {
+		Action      string
+		PullRequest struct {
+			Title string
+			URL   string `json:"html_url"`
+		} `json:"pull_request"`
+		Issue struct {
+			Title string
+			URL   string `json:"html_url"`
+		}
+		Review struct {
+			URL string `json:"html_url"`
+		}
+		Comment struct {
+			URL string `json:"html_url"`
+		}
+	}
+}
+
+type Events []*Event
